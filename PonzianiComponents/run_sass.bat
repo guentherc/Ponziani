037237d1efc7249d61cb0da09672e6f9d445ecd6
guentherc <chr.guenther@web.de>
2021-09-16 19:30:28 +0200

    Get rid of Delegate.Sass

---
+++ PonzianiComponents/run_sass.bat
@@ -0,0 +1,2 @@
+call "D:\Program Files\dart-sass\sass" --no-source-map Scoresheet.razor.scss Scoresheet.razor.css
+call "D:\Program Files\dart-sass\sass" --no-source-map Chessboard.razor.scss Chessboard.razor.css
